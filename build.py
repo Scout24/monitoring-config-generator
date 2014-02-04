9e3c285148eebc13b96a7291a4dff07fccef6e91
Valentin Haenel <valentin.haenel@gmx.de>
2014-02-04 16:03:15 +0100

    move requirements to build.py

--- build.py
+++ build.py
@@ -1,11 +1,16 @@
 
-from pybuilder.core import use_plugin
+from pybuilder.core import use_plugin, init
 
 use_plugin("python.core")
 use_plugin("python.unittest")
+use_plugin('python.install_dependencies')
 
 name = "monitoring-config-generator"
 default_task = "publish"
 
 
 
+@init
+def set_properties(project):
+    project.depends_on('pyyaml')
+    project.depends_on('httplib2')
