bf6b7f183aa353e43a295e8de7855808f3aa0378
Valentin Haenel <valentin.haenel@gmx.de>
2014-02-04 17:11:55 +0100

    fixup the metadata

--- build.py
+++ build.py
@@ -9,12 +9,12 @@ use_plugin('python.distutils')
 name = "monitoring-config-generator"
 default_task = "publish"
 
-authors = [Author('Schlomo Schapiro, Sven Schindler')]
-license="GPL",
-description="Get monitoring configuration in YAML format via HTTP and generate icinga host and check config",
+authors = [Author('Schlomo Schapiro', ''), Author('Sven Schindler', '')]
+license="GPL"
+description="Get monitoring configuration in YAML format via HTTP and generate icinga host and check config"
 summary = "Fluent interface facade for Michael Foord's mock."
 version = '4'
-url="https://github.com/ImmobilienScout24/monitoring-config-generator.git",
+url="https://github.com/ImmobilienScout24/monitoring-config-generator.git"
 
 
 @init
