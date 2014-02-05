f11b5ecd14d1ffa8e34e11b3a599f68ef29e6128
Valentin Haenel <valentin.haenel@gmx.de>
2014-02-05 14:11:18 +0100

    start measuring test coverage with pybuilder

--- build.py
+++ build.py
@@ -5,6 +5,7 @@ use_plugin("python.core")
 use_plugin("python.unittest")
 use_plugin('python.install_dependencies')
 use_plugin('python.distutils')
+use_plugin('python.coverage')
 use_plugin('copy_resources')
 
 name = "monitoring-config-generator"
