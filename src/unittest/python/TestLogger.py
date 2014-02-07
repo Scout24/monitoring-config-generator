0ab2be5d4501f683e53b5d198e4771000217145a
Valentin Haenel <valentin.haenel@gmx.de>
2014-02-07 16:13:49 +0100

    fix the test logger message, seems it was copied from the yaml_server

--- src/unittest/python/TestLogger.py
+++ src/unittest/python/TestLogger.py
@@ -1,9 +1,10 @@
 import logging
 
+
 def init_test_logger():
     """set up logging for testing without syslog by logging DEBUG to console"""
     logger = logging.getLogger()
     loghandler = logging.StreamHandler()
-    loghandler.setFormatter(logging.Formatter('yaml_server[%(filename)s:%(lineno)d]: %(levelname)s: %(message)s'))
+    loghandler.setFormatter(logging.Formatter('monconfgenerator[%(filename)s:%(lineno)d]: %(levelname)s: %(message)s'))
     logger.addHandler(loghandler)
     logger.setLevel(logging.DEBUG)
