bc5429a4cd0c8b50e4a8a50878e1c57c867969a1
Valentin Haenel <valentin.haenel@gmx.de>
2014-02-07 10:30:24 +0100

    nuke the InputReader

--- src/unittest/python/reader_tests.py
+++ src/unittest/python/reader_tests.py
@@ -5,45 +5,13 @@ import unittest
 from mock import patch, Mock
 
 
-from TestLogger import init_test_logger
-
 os.environ['MONITORING_CONFIG_GENERATOR_CONFIG'] = "testdata/testconfig.yaml"
-from monitoring_config_generator.readers import InputReader
 from monitoring_config_generator.readers import (read_config,
                                                  read_config_from_file,
                                                  read_config_from_host,
                                                  read_etag,
                                                  )
 from monitoring_config_generator.exceptions import MonitoringConfigGeneratorException
-
-
-class Test(unittest.TestCase):
-
-    init_test_logger()
-
-    def test_etag_no_input(self):
-        input_reader = InputReader("test.some.domain.yaml", "testDirName")
-        input_reader.read_input()
-        self.assertEquals(None, input_reader.etag)
-
-    def test_read_file(self):
-        host_name = "testhost03"
-        yaml_filename = host_name + ".yaml"
-        cfg_filename = host_name + ".cfg"
-        input_dir = "testdata/itest_testhost03_new_format"
-        full_input_path = os.path.join(input_dir, yaml_filename)
-        output_dir = "testOutputDir"
-        full_output_path = os.path.join(output_dir, cfg_filename)
-        input_reader = InputReader(full_input_path, output_dir)
-        input_reader.read_input()
-        self.assertEquals(host_name, input_reader.hostname)
-        self.assertEquals(full_input_path, input_reader.filename)
-        self.assertEquals(full_output_path, input_reader.output_path)
-        self.assertTrue(input_reader.config_changed)
-        self.assertTrue(input_reader.is_file)
-
-        # output dir doesn't exist, so there should be no etag
-        self.assertEquals(None, input_reader.etag)
 
 
 class TestReadEtag(unittest.TestCase):
