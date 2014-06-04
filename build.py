b66c0936a3fd84a41ac5f1fc775006eabbaa65be
Valentin Haenel <valentin.haenel@gmx.de>
2014-06-04 14:17:46 +0200

    minor fixes to build.py

--- build.py
+++ build.py
@@ -20,11 +20,11 @@ authors = [Author('Schlomo Schapiro', ''),
            Author('Valentin Haenel', ''),
            Author('Marco Hoyer', ''),
            ]
-license="GPL"
-description="Get monitoring configuration in YAML format via HTTP and generate icinga host and check config"
-summary = "Fluent interface facade for Michael Foord's mock."
+license = "GPL"
+description = "Get monitoring configuration in YAML format via HTTP and generate icinga host and check config"
+summary = description
 version = '5'
-url="https://github.com/ImmobilienScout24/monitoring-config-generator.git"
+url = "https://github.com/ImmobilienScout24/monitoring-config-generator.git"
 
 
 @init
@@ -41,6 +41,7 @@ def set_properties(project):
     project.set_property('copy_resources_target', '$dir_dist')
     project.get_property('copy_resources_glob').extend(
             ['setup.cfg', 'LICENSE.TXT', 'README.md', 'MANIFEST.in'])
+
 
 @init(environments='teamcity')
 def set_properties_for_teamcity_builds(project):
